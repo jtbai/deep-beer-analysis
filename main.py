ac2ec416bc8d1dc250e7908f383388e7d320689a
Jean-Thomas Baillargeon <jtbaillargeon@hotmail.com>
2019-09-13 15:15:57 -0400

    un main qui marche meme pas

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from repository.mongo_extractor import MongoExtractor
 
 MONGO_CONGIF_FILE_PATH = "config/mongo_connection_details.json"
 mongo_extractor = MongoExtractor.get_connection(json.load(open(MONGO_CONGIF_FILE_PATH)))
+
+
+for checking in mongo_extractor.get_all_checking_with_tags():
+    pass
