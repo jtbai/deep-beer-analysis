834e72beeca1aaa10d6bb23fb4c3867871584fed
Jean-Thomas Baillargeon <jtbaillargeon@hotmail.com>
2019-09-20 14:49:15 -0400

    bootstrap et les caractères funky

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ from ml.beer_to_taste_skipgram import BeerToTasteSkipgram
 from repository.mongo_beer_extractor import MongoBeerExtractor
 from repository.mongo_checkin_extractor import MongoCheckinExtractor
 import re
+import html
+
 
 BEER_MONGO_CONGIF_FILE_PATH = "config/beer_mongo_connection_details.json"
 CHECKIN_MONGO_CONGIF_FILE_PATH = "config/checkin_mongo_connection_details.json"
@@ -32,6 +34,13 @@ def format_top_taste(tastes):
 
     return output
 
+def generate_html_header(title):
+    html_header = "<html><head><title>beer2vec: {}</title></head>".format(title)
+    html_header +='<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">'
+    html_header += "<body>"
+    return html_header
+
+
 def format_dictionary_for_select_dropdown(idx_to_value):
     options = "<option value=-1></option>"
     options += " ".join(["<option value={}>{}</option>".format(idx, value) for idx, value in idx_to_value.items()])
@@ -56,13 +65,13 @@ def prepare_request_data_for_requested_profile(request_data):
 
 def format_beer_with_base_info(beers):
     str = "<ul>{}</ul>"
-    beer_strs = ["<li><b>{0}</b>: <a href='/beer/{1}'>{1}</a> ({2}) | {3}</li>".format(beer_extractor.get_beer_brewery(beer), beer, beer_extractor.get_beer_type(beer), beer_extractor.get_beer_score(beer)) for beer in beers]
+    beer_strs = ["<li><b>{0}</b>: <a href='/beer/{1}'>{1}</a> ({2}) | {3}</li>".format(beer_extractor.get_beer_brewery(beer), html.escape(beer), beer_extractor.get_beer_type(beer), beer_extractor.get_beer_score(beer)) for beer in beers]
     beer_strs.sort()
     return str.format(" ".join(beer_strs))
 
 def format_beer_list(beers):
     str = "<ul>{}</ul>"
-    beer_strs = ["<li><a href='/beer/{0}'>{0}</a></li>".format(beer) for beer in beers]
+    beer_strs = ["<li><a href='/beer/{0}'>{0}</a></li>".format(html.escape(beer)) for beer in beers]
     return str.format(" ".join(beer_strs))
 
 def format_checkin(checkin_list):
@@ -74,9 +83,9 @@ def format_checkin(checkin_list):
 def home():
     local = request.args.get("local", "")
     local_beers = beer_extractor.get_local_beers(local)
-    html_output = "<html><head><title>beer2vec</title></head><body>" \
-                  "<h1><a href=/similar_beer>Build a beer</a></h1>" \
-                  "<h1><a href=/?local=QC>Québec</a> | <a href=/?local=Canada>Canada</a> | <a href=/?local=VT>Vermont</a> | <a href=/?local=>All</a></h1>"
+    html_output = generate_html_header("Accueil")
+    html_output += "<h1><a href=/similar_beer>Build a beer</a></h1>" \
+                  "<h2><a href=/?local=QC>Québec</a> | <a href=/?local=Canada>Canada</a> | <a href=/?local=Belgium>Belgique</a> | <a href=/?local=VT>Vermont</a> | <a href=/?local=>All</a></h2>"
     html_output += format_beer_with_base_info([b for b, _ in beer_to_idx.items() if b in local_beers])
     html_output += "</body></html>"
     return html_output
@@ -95,7 +104,8 @@ def similar_beer():
         local_similar_beer = [(name, score) for name, score in most_similar_beer if name in local_beers]
         local_similar_beer.sort(key=lambda x: x[1], reverse=True)
 
-        output = "<a href=/>Home</a>" \
+        output = generate_html_header("Build a beer")
+        output += "<a href=/>Home</a>" \
                  "<h1><a href=/similar_beer>Build a beer</a></h1>".format()
         output += "<h2>Requested Profile</h2>"
         output += format_top_taste([(model.idx_to_tag[idx], proportion) for idx, proportion in requested_profile])
@@ -103,8 +113,8 @@ def similar_beer():
         output += format_beer_with_base_info([b for b, _ in local_similar_beer[0:10]])
 
     elif request.method == "GET":
-
-        output = "<a href=/>Home</a>" \
+        output = generate_html_header("Build a beer")
+        output += "<a href=/>Home</a>" \
                  "<h1><a href=/similar_beer>Build a beer</a></h1>".format()
         output += "<h2>Requested Profile</h2>"
         output += "<form name=request_profile method=POST action=/similar_beer>"
@@ -132,22 +142,22 @@ def beer(name):
 
     similar_beers = [b for b, _ in similar_local_beer][-10:]
     dissimilar_beers = [b for b, _ in similar_local_beer][:10]
+    output = generate_html_header(name)
+    output += '<a href=/>Home</a>  ' \
+    '<h1>{} - {} ({})</h1>' \
+    '<h1>Taste profile</h1>' \
+    '{}' \
+    '<h1>User Reviews</h1>' \
+    '<h2>Happy people</h2>{}' \
+    '<h2>Sad people</h2>{}' \
+    '<h1>Beer Comparison</h1>' \
+    '<h2>Similar:</h2> {}  ' \
+    '<h2>Dissimilar:</h2> {}'.format(beer_extractor.get_beer_brewery(name), name, beer_extractor.get_beer_type(name),
+                                     format_top_taste(flavors),
+                                     format_checkin(review[-10:]), format_checkin(review[:10]),
+                                     format_beer_with_base_info(similar_beers),
+                                     format_beer_with_base_info(dissimilar_beers))
 
-    return '<html><head><title>beer2vec</title></head><body>' \
-           '<a href=/>Home</a>  ' \
-           '<h1>{} - {} ({})</h1>' \
-           '<h1>Taste profile</h1>' \
-           '{}'\
-           '<h1>User Reviews</h1>' \
-           '<h2>Happy people</h2>{}'\
-           '<h2>Sad people</h2>{}' \
-           '<h1>Beer Comparison</h1>' \
-           '<h2>Similar:</h2> {}  ' \
-           '<h2>Dissimilar:</h2> {}'.format(beer_extractor.get_beer_brewery(name), name, beer_extractor.get_beer_type(name),
-                                            format_top_taste(flavors),
-                                            format_checkin(review[-10:]),format_checkin(review[:10]),
-                                            format_beer_with_base_info(similar_beers),
-                                            format_beer_with_base_info(dissimilar_beers))
-
+    return output
 if __name__== "__main__":
     app.run(debug=False, host="0.0.0.0", port=5100)
